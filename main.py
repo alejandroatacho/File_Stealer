b67416d7760423356482d597cbb8b645589b4643
Hinamizawa <themoonbunnys@gmail.com>
2022-06-15 10:10:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import os
+from shutil import copyfile
+
 
 # directory = r'temp/file_path'
 directory = r'C:\Users\alex_\Desktop\Holder\School\Semester 4\Economics\assignment'
-
+save_path = r'temp/save_path'
 for filename in os.listdir(directory):
     if filename.endswith(".pdf") or filename.endswith(".pptx") or filename.endswith(".doc") or filename.endswith(".docx"):
         print(filename)
+        copyfile(directory + '/' + filename, save_path + '/' + filename)
+        # file = os.path.basename(filename[0])
+        # copyfile(file[0], os.path.join(save_path, file))
 
     else:
         print(
