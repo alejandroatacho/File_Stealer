948a786c63d160bad4062aaf5fa02fb54d45fa90
Hinamizawa <themoonbunnys@gmail.com>
2022-06-15 10:15:43 -0400

    update

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from shutil import copyfile
 
 # directory = r'temp/file_path'
 directory = r'C:\Users\alex_\Desktop\Holder\School\Semester 4\Economics\assignment'
-save_path = r'temp/save_path'
+save_path = r'C:\Users\alex_\Documents\GitHub\FileStealer\File_Stealer\temp\save_path'
 for filename in os.listdir(directory):
     if filename.endswith(".pdf") or filename.endswith(".pptx") or filename.endswith(".doc") or filename.endswith(".docx"):
-        print(filename)
         copyfile(directory + '/' + filename, save_path + '/' + filename)
+        print(filename + " has been successfully copied!")
         # file = os.path.basename(filename[0])
         # copyfile(file[0], os.path.join(save_path, file))
 
