6113eef1e62e7eeed771bacbc3b992e972203e97
Hinamizawa <hinamizawa.github@gmail.com>
2022-09-06 12:39:37 -0400

    uu

--- main.py
+++ main.py
@@ -20,45 +20,49 @@ save_path = r'temp/file_path'
 
 def run():
     dirs = os.listdir(directory)
-
+    # isdir = os.path.isdir(directory)
     for filename in dirs:
+        if subdirs == os.path.isdir(filename):
+            for subdirs in dirs:
+                if subdirs.endswith(".pptx"):
+                    print(subdirs)
+                    copyfile(subdirs, save_path)
+                elif subdirs.endswith(".doc"):
+                    print(subdirs)
+                    copyfile(subdirs, save_path)
+                elif subdirs.endswith(".docx"):
+                    print(subdirs)
+                    copyfile(subdirs, save_path)
+                elif subdirs.endswith(".txt"):
+                    print(subdirs)
+                    copyfile(subdirs, save_path)
+                elif subdirs.endswith(".pdf"):
+                    print(subdirs)
+                    copyfile(subdirs, save_path)
+                elif subdirs.endswith(".mp3"):
+                    print(subdirs)
+                    copyfile(subdirs, save_path)
+                else:
+                    print("No files found")
+                    break
+
+        # print(filename)
+        # copyfile(filename, save_path)
         # comment below is broken due to max 3 arguments per .endswith, very easy fix but im also very lazy to change 1 line of code :3
         # if filename.endswith(".pptx", ".doc", ".docx", ".txt", ".pdf", ".mp3"):
-        for subdirs in dirs:
-            if subdirs.endswith(".pptx"):
-                print(subdirs)
-                copyfile(subdirs, save_path)
-            elif subdirs.endswith(".doc"):
-                print(subdirs)
-                copyfile(subdirs, save_path)
-            elif subdirs.endswith(".docx"):
-                print(subdirs)
-                copyfile(subdirs, save_path)
-            elif subdirs.endswith(".txt"):
-                print(subdirs)
-                copyfile(subdirs, save_path)
-            elif subdirs.endswith(".pdf"):
-                print(subdirs)
-                copyfile(subdirs, save_path)
-            elif subdirs.endswith(".mp3"):
-                print(subdirs)
-                copyfile(subdirs, save_path)
-            else:
-                print("No files found")
-                break
-        if filename.endswith(".pdf") or filename.endswith(".pptx") or filename.endswith(".doc") or filename.endswith(".docx"):
-            copyfile(directory + '/' + filename, save_path + '/' + filename)
-            print(filename + " has been successfully copied!")
+        # if filename.endswith(".pdf") or filename.endswith(".pptx") or filename.endswith(".doc") or filename.endswith(".docx"):
+        #     copyfile(directory + '/' + filename, save_path + '/' + filename)
+        #     print(filename + " has been successfully copied!")
 
-            # file = os.path.basename(filename[0])
-            # copyfile(file[0], os.path.join(save_path, file))
+        #     # file = os.path.basename(filename[0])
+        #     # copyfile(file[0], os.path.join(save_path, file))
 
-        else:
-            print(
-                "This is not a an important file, this file will be skipped: " + filename + "!")
-            text_file = open("output.txt", "a")
-            n = text_file.write(filename + "\n")
-            text_file.close()
+        # else:
+        #     print(
+        #         "This is not a an important file, this file will be skipped: " + filename + "!")
+        #     text_file = open("output.txt", "a")
+        #     n = text_file.write(filename + "\n")
+        #     text_file.close()
 
 
 __name__ == "__main__"
