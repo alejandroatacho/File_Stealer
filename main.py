da3f598730ba6b6cd2c30a85f30181714cf7e34d
Hinamizawa <hinamizawa.github@gmail.com>
2022-09-07 09:46:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -19,50 +19,24 @@ save_path = r'temp/file_path'
 
 
 def run():
-    dirs = os.listdir(directory)
-    # isdir = os.path.isdir(directory)
-    for filename in dirs:
-        if subdirs == os.path.isdir(filename):
-            for subdirs in dirs:
-                if subdirs.endswith(".pptx"):
-                    print(subdirs)
-                    copyfile(subdirs, save_path)
-                elif subdirs.endswith(".doc"):
-                    print(subdirs)
-                    copyfile(subdirs, save_path)
-                elif subdirs.endswith(".docx"):
-                    print(subdirs)
-                    copyfile(subdirs, save_path)
-                elif subdirs.endswith(".txt"):
-                    print(subdirs)
-                    copyfile(subdirs, save_path)
-                elif subdirs.endswith(".pdf"):
-                    print(subdirs)
-                    copyfile(subdirs, save_path)
-                elif subdirs.endswith(".mp3"):
-                    print(subdirs)
-                    copyfile(subdirs, save_path)
-                else:
-                    print("No files found")
-                    break
+    for root, subdirectories, files in os.walk(directory):
+        for subdirectory in subdirectories:
+            print(os.path.join(root, subdirectory))
+            if subdirectory.endswith('.txt') or subdirectory.endswith('.pdf') or subdirectory.endswith('.docx'):
+                copyfile(os.path.join(root, subdirectory), save_path)
 
+        for file in files:
+            print(os.path.join(root, file))
+            if file.endswith('.txt') or file.endswith('.docx') or file.endswith('.pdf'):
+                print('File found')
+                print(file)
+                print(os.path.join(root, file))
+                copyfile(os.path.join(root, file),
+                         os.path.join(save_path, file))
+                print('File copied')
         # print(filename)
         # copyfile(filename, save_path)
         # comment below is broken due to max 3 arguments per .endswith, very easy fix but im also very lazy to change 1 line of code :3
-        # if filename.endswith(".pptx", ".doc", ".docx", ".txt", ".pdf", ".mp3"):
-        # if filename.endswith(".pdf") or filename.endswith(".pptx") or filename.endswith(".doc") or filename.endswith(".docx"):
-        #     copyfile(directory + '/' + filename, save_path + '/' + filename)
-        #     print(filename + " has been successfully copied!")
-
-        #     # file = os.path.basename(filename[0])
-        #     # copyfile(file[0], os.path.join(save_path, file))
-
-        # else:
-        #     print(
-        #         "This is not a an important file, this file will be skipped: " + filename + "!")
-        #     text_file = open("output.txt", "a")
-        #     n = text_file.write(filename + "\n")
-        #     text_file.close()
 
 
 __name__ == "__main__"
