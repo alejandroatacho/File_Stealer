ecc4acb0d74e8cd9a9b6443b0a45b9d96d61fc2b
Hinamizawa <hinamizawa.github@gmail.com>
2022-09-09 12:00:14 -0400

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,22 @@
+b = 2400
+a = 100
+c = 800
+d = 400
+e = 1200
+
+f = 3
+g = 1.5
+h = 1
+i = 0.5
+j = 1.5
+k = 1
+
+def price():
+    print("Total Price: ", b + a + c + d + e)
+
+def total_days():
+    print("total days: ",f + g + h + i + j + k)
+
+"__init__" == "__main__"
+price()
+total_days()
